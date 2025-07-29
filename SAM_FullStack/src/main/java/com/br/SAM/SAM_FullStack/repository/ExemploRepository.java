66e0a4861b2816558586414b9c744e829cfdbf16
unknown <marcolinvh@gmail.com>
2025-07-28 21:09:23 -0300

    Subindo estrutura base

---
+++ SAM_FullStack/src/main/java/com/br/SAM/SAM_FullStack/repository/ExemploRepository.java
@@ -0,0 +1,4 @@
+package com.br.SAM.SAM_FullStack.repository;
+
+public class ExemploRepository {
+}
---
+++ SAM_FullStack/src/test/java/com/br/SAM/SAM_FullStack/SamFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.SAM.SAM_FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
