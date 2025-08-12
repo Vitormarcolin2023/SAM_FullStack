6f58e9df4fbc0ac7d60ecc91f7c3bb00a05c5c64
unknown <marcolinvh@gmail.com>
2025-08-12 19:18:47 -0300

    Subindo os serviços de delatar e update

--- SAM_FullStack/src/main/java/com/br/SAM/SAM_FullStack/service/AlunoService.java
+++ SAM_FullStack/src/main/java/com/br/SAM/SAM_FullStack/service/AlunoService.java
@@ -23,9 +23,21 @@ public class AlunoService {
         return alunoRepository.findAll();
     }
 
+    public Aluno findById(Integer id){
+        return alunoRepository.getAluno(id);
+    }
+
     public Aluno save(Aluno aluno){
         emailService.enviarEmailTexto(aluno.getEmail(), "Novo aluno cadastrado", "Aluno: "+ aluno.getNome()+", RA: "+ aluno.getRa() +" cadastrado com sucesso");
         return  alunoRepository.save(aluno);
+    }
+
+    public void delete(Integer id){
+        alunoRepository.delete(id);
+    }
+
+    public Aluno update(Integer id, Aluno alunoUpdate){
+        return alunoRepository.update(id, alunoUpdate);
     }
 }
 
