da5e3a5d8584b306b02bb8c1ea5e7ade3a5dcd23
cecimoura <cecimoura91@gmail.com>
2025-08-13 19:43:22 -0300

    teste

--- SAM_FullStack/src/main/java/com/br/SAM/SAM_FullStack/model/Mentor.java
+++ SAM_FullStack/src/main/java/com/br/SAM/SAM_FullStack/model/Mentor.java
@@ -1,4 +1,13 @@
 package com.br.SAM.SAM_FullStack.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsContructor
+@NoArgsContructor
+@Table(name= "tb_mentor")
 
 public class Mentor {
+
 }
