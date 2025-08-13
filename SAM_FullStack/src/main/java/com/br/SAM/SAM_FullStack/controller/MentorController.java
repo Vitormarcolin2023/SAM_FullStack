e83a4d1c816520e52cfc030ebb55d76f451c6342
cecimoura <cecimoura91@gmail.com>
2025-08-13 13:21:57 -0300

    começando o MentorController

--- SAM_FullStack/src/main/java/com/br/SAM/SAM_FullStack/controller/MentorController.java
+++ SAM_FullStack/src/main/java/com/br/SAM/SAM_FullStack/controller/MentorController.java
@@ -1,4 +1,38 @@
 package com.br.SAM.SAM_FullStack.controller;
 
+import com.br.SAM.SAM_FullStack.model.MentorEntity;
+import com.br.SAM.SAM_FullStack.service.MentorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mentores")
 public class MentorController {
+
+    private final MentorService mentorService;
+
+    public MentorController(MentorService mentorService){
+        this.mentorService = mentorService;
+    }
+
+    //listar todos
+    @GetMapping
+    public ResponseEntity<List<MentorEntity>> listAll(){
+        return ResponseEntity.ok(mentorService.listAll());
+    }
+
+
+
+
+
+
+
+
+
+
 }
