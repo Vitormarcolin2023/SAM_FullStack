64a437bc2a86c653557337f99662b75a970954f4
cecimoura <cecimoura91@gmail.com>
2025-08-14 10:04:50 -0300

    AreaDeAtuacao criada

---
+++ sam/src/main/java/com/api/sam/repository/AreaDeAtuacaoRepository.java
@@ -0,0 +1,7 @@
+package com.api.sam.repository;
+
+import com.api.sam.model.AreaDeAtuacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaDeAtuacaoRepository extends JpaRepository<AreaDeAtuacao, Long> {
+}
